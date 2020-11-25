6745bad8f94fd647c809e4877a59ea7c93081c9b
Jakub Dudziński <jdudzinski101@gmail.com>
2020-11-25 19:37:14 +0100

    hamlet.txt added

--- lab04/z1.c
+++ lab04/z1.c
@@ -4,27 +4,48 @@
 
 #define BUF_SIZE 256
 
-int main() {
+int main(int argc, char** argv) {
 
     pid_t pid;
     int fd[2], in_fd, n;
     char buf[BUF_SIZE];
-    char fname[32];
+    char* fname = NULL;
+    
+    if(argc > 1)
+    {
+    	printf("No filename as program's argument!\n");
+		return 1;
+	}
     
     if(pipe(fd) < 0)
     {
         fprintf(stderr, "Failed creating pipe\n");
-        return 1;
+        return 2;
     }
 
     pid = fork();
 
     if(pid == 0)
     {
-        printf("Child created!\n");
+    	close(fd[1]);
+        
     }
     else
     {
-        printf("Back in parent\n");
+    	close(fd[0]);
+    
+        if((in_fd = open(fname, O_RDONLY)) < 0)
+        {
+            fprintf(stderr, "Failed opening file\n");
+            return 3;
+        }
+        
+        while((n = read(in_fd, &buf, BUF_SIZE)) > 0)
+        {
+            if(write(fd[1], &buf, n) < 0) {
+                fprintf(stderr, "Failed writing to pipe\n");
+                return 4;
+            }  
+        }
     } 
 }
